0f0b8676aca0e703f80afc0aa1dc8c046dd3a881
Manikant Jha <manikantcodes@gmail.com>
2025-01-18 11:29:45 +0530

    useState hook

--- src/components/home/faqs/FaqsListItem.jsx
+++ src/components/home/faqs/FaqsListItem.jsx
@@ -1,21 +1,43 @@
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./faqsRow.module.css";
 
 function FaqsListItem(props) {
   const { question, answer, index } = props;
-  function myFunction(event) {
-    const para = document.getElementById(`my-para-${index}`);
-    const btn = document.getElementById(`my-btn-${index}`);
 
-    if (para.style.display === "block") {
-      para.style.display = "none";
-      btn.style.rotate = "0deg";
+  // function myFunction(event) {
+  //   const para = document.getElementById(`my-para-${index}`);
+  //   const btn = document.getElementById(`my-btn-${index}`);
+
+  //   if (para.style.display === "block") {
+  //     para.style.display = "none";
+  //     btn.style.rotate = "0deg";
+  //   } else {
+  //     para.style.display = "block";
+  //     btn.style.rotate = "180deg";
+  //   }
+  // }
+
+  // let showPara = false;
+
+  // const stateArr = useState(false);
+  // const showPara = stateArr[0];
+  // const setShowPara = stateArr[1];
+  // OR
+
+  const [showPara, setShowPara] = useState(false);
+
+  function myFunction(e) {
+    if (showPara) {
+      // showPara = false;
+      setShowPara(false);
     } else {
-      para.style.display = "block";
-      btn.style.rotate = "180deg";
+      // showPara = true;
+      setShowPara(true);
     }
+
+    console.log("showPara", showPara);
   }
 
   return (
@@ -23,15 +45,21 @@ function FaqsListItem(props) {
       <p>
         <span>{question}</span>{" "}
         <button id={`my-btn-${index}`} onClick={myFunction}>
-          <FontAwesomeIcon icon={faArrowDown} />
+          {showPara ? (
+            <FontAwesomeIcon icon={faArrowUp} />
+          ) : (
+            <FontAwesomeIcon icon={faArrowDown} />
+          )}
         </button>
         {/* <button onclick="myFunction()">
           <FontAwesomeIcon icon={faArrowDown} />
         </button> */}
       </p>
-      <p id={`my-para-${index}`} style={{ display: "block" }}>
-        {answer}
-      </p>
+      {showPara && (
+        <p id={`my-para-${index}`} style={{ display: "block" }}>
+          {answer}
+        </p>
+      )}
     </div>
   );
 }
