af5cb7ddf225494d8d3c9cece01c171c2eda3ffb
Manikant Jha <manikantcodes@gmail.com>
2025-01-24 11:24:38 +0530

    removed imports

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Navbar from "../../components/common/navbar/Navbar";
-import Footer from "../../components/common/footer/Footer";
 import Title from "../../components/common/Title";
 import styles from "./contact.module.css";
 
